af32d46bb87f89270c059c6b7856bdb4509cbea6
Anton <bazhan.anton@ukr.net>
2020-10-13 14:57:44 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = n * -1;
+    }
+    const stringNumber = n + "";
+    const arrayNumber = stringNumber.split("").reverse();
+    console.log(arrayNumber);
+    const number = arrayNumber.join("");
+    return Number(number);
+};
